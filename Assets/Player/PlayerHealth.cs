dc622e31b358fa18d75224616a90369ce5ed15c6
irene-portillo <irenemaideli@gmail.com>
2024-04-22 21:14:16 -0400

    Edited PlayerHealth: Created main variables
    Created main variables for health and initialized them in Unity Game Engine

---
+++ Assets/Player/PlayerHealth.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int plrHealth;
+    public BoxCollider2D plrHitBoxBody;
+    public BoxCollider2D plrHitBoxFt;
+}
