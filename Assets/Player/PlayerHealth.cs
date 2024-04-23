dea43c6f7aa3b7ed7a4685118d150e8380c4976b
irene-portillo <irenemaideli@gmail.com>
2024-04-22 21:15:40 -0400

    Edited PlayerHealth: OnCollisionEnter2D(Collision2D collision)
    Made it check if player collided with an object in the 'Spore' layer

--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -7,4 +7,23 @@ public class PlayerHealth : MonoBehaviour
     public int plrHealth;
     public BoxCollider2D plrHitBoxBody;
     public BoxCollider2D plrHitBoxFt;
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Damage plr if touches spore
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Spore")){
+            decreaseHealth(5);
+        }
+    }
+
+    private void decreaseHealth(int increment)
+    {
+        plrHealth -= increment;
+    }
 }
