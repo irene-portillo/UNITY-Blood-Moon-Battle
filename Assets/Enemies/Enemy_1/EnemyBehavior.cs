78f8affcff8c1e2dd241414f9beedec9036ca949
irene-portillo <irenemaideli@gmail.com>
2024-04-23 10:42:22 -0400

    Created EnemyBehavior script
    Created EnemyBehavior script to manage how the enemy will behave: shoot spores and may add movement here too

---
+++ Assets/Enemies/Enemy_1/EnemyBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
