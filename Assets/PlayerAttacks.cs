91b93127030fdf1eac548f4c85eb39fd441f82bb
irene-portillo <irenemaideli@gmail.com>
2024-04-23 22:37:37 -0400

    Edited PlayerAttacks: Declared + Initialized main variables
    Declared variables for different sprites and logic necessary to get task complete.
    
    Then initialized them on script + Unity Game Engine

--- Assets/PlayerAttacks.cs
+++ Assets/PlayerAttacks.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
+    //SPRITE
+    private SpriteRenderer spriteRendererPlr;
+    public GameObject spriteObjHolder;
+    public Sprite idlePlr;
+    public Sprite attackPlr;
+    //LOGIC
+    public float attackRate; 
+    private float attackTimer = 0;
+    private bool isAttacking = false;
+    private GameObject enemyObject; // set through collision 
+
     void Start()
     {
     }
