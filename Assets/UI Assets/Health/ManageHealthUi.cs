e8ace04106642279180bc0ea56b9d42ee4c326f8
irene-portillo <irenemaideli@gmail.com>
2024-04-25 03:35:15 -0400

    Edited ManageHealthUi: Set up necessary variables
    Declared / initialized necessary variables

--- Assets/UI Assets/Health/ManageHealthUi.cs
+++ Assets/UI Assets/Health/ManageHealthUi.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManageHealthUi : MonoBehaviour
 {
+    public Text healthStatus;
+    public PlayerHealth playerHealthScript;
+    public Animator heartAnimator;
+
     void Start(){}
 
     void Update()
