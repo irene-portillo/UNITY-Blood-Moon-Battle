e437d5d71a9b8e08c0b964560bd350518173da0b
irene-portillo <irenemaideli@gmail.com>
2024-04-24 20:17:48 -0400

    Scripted death animation [Player]
    Made checkPlrHealth() to check if the player's health reaches 0. Then , the player will have died so play death animation

--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -8,11 +8,15 @@ public class PlayerHealth : MonoBehaviour
     public BoxCollider2D plrHitBoxBody;
     public BoxCollider2D plrHitBoxFt;
 
+    public Animator plrAnimtor;
+
     // Start is called before the first frame update
     void Start(){}
 
     // Update is called once per frame
-    void Update(){}
+    void Update(){
+        checkPlrHealth();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,5 +29,14 @@ public class PlayerHealth : MonoBehaviour
     private void decreaseHealth(int increment)
     {
         plrHealth -= increment;
+    }
+
+    void checkPlrHealth()
+    {
+        if (plrHealth <= 0)
+        {
+            Debug.Log("Player has DIED!!!!");
+            plrAnimtor.SetBool("plrIsDead", true); // update health for anim
+        }
     }
 }
